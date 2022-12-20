31173b655e3ee9e9e0ae0d2dd75c9f2a60aad8f9
Chijioke Chika <8pluscreative@gmail.com>
2022-12-20 08:00:14 +0100

    APIs

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,1 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const Api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+});
