edd229bd9be886255d62cab0f9840b96daf8003c
Chijioke Chika <8pluscreative@gmail.com>
2022-12-22 16:02:14 +0100

    Geography

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Dashboard from "./screens/Dashboard/Dashboard";
 import Products from "./screens/Products/Products";
 import Customers from "./screens/Customers/Customers";
 import Transactions from "./screens/Transactions/Transactions";
+import Geography from "./screens/Geography/Geography";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -26,6 +27,7 @@ function App() {
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
             </Route>
           </Routes>
         </ThemeProvider>
