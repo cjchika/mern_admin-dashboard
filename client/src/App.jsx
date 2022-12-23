b7639a192a8b9d7f045f90601178c463f2fdfd65
Chijioke Chika <8pluscreative@gmail.com>
2022-12-23 07:39:55 +0100

    Daily Sales

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Customers from "./screens/Customers/Customers";
 import Transactions from "./screens/Transactions/Transactions";
 import Geography from "./screens/Geography/Geography";
 import SalesOverview from "./screens/SalesOverview/SalesOverview";
+import Daily from "./screens/Daily/Daily";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -30,6 +31,7 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<SalesOverview />} />
+              <Route path="/daily" element={<Daily />} />
             </Route>
           </Routes>
         </ThemeProvider>
