54678e9bf6cbabbc1684f4f0f887f0bfbe676edc
Chijioke Chika <8pluscreative@gmail.com>
2022-12-24 14:08:44 +0100

    Dashboard

--- client/src/screens/Dashboard/Dashboard.jsx
+++ client/src/screens/Dashboard/Dashboard.jsx
@@ -1,7 +1,69 @@
 import React from "react";
+import FlexBetween from "../../components/FlexBetween";
+import Header from "../../components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { useGetDashboardQuery } from "../../state/api";
+import { DataGrid } from "@mui/x-data-grid";
+import BreakdownChart from "../../components/BreakdownChart";
+import OverviewCharts from "../../components/OverviewCharts";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+  const { data, isLoading } = useGetDashboardQuery();
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "No. of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${+params?.value}`,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header
+        title="DASHBOARD"
+        subTitle="You have the power to make things happen."
+      />
+    </Box>
+  );
 };
 
 export default Dashboard;
