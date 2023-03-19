42c6686bfdced117f270ae64b38f11c7efede0d4
Chijioke Chika <8pluscreative@gmail.com>
2023-03-19 13:47:32 +0100

    Deployment

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const Api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-dash-api.onrender.com" }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
